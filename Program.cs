ce8a265ada538a845b5e1a30e75f944e5da6c3e8
Luke Wardle <l.wardle@live.co.uk>
2022-04-23 17:08:12 +0100

    pizza selection funtion order and cancel buttons

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
